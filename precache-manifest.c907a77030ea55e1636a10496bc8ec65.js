4d03e129cca24320e42af9d86252ebd628c74988
홍현경 <nowhhk@gmail.com>
2020-05-07 22:29:54 +0900

    Updates

---
+++ precache-manifest.c907a77030ea55e1636a10496bc8ec65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b517cb0d7034d827b52160f36546fb0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7ab58ad59360bf205f89",
+    "url": "/movie_app/static/css/main.3fff2748.chunk.css"
+  },
+  {
+    "revision": "f5959358a11e6d0a7660",
+    "url": "/movie_app/static/js/2.df5064f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.df5064f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab58ad59360bf205f89",
+    "url": "/movie_app/static/js/main.c814195e.chunk.js"
+  },
+  {
+    "revision": "be85ea99d5bf5ec654f0",
+    "url": "/movie_app/static/js/runtime-main.00c5d7b4.js"
+  }
+]);
